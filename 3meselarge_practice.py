e22874d87526f3f1d1fa34e9839bae4c406a2f15
navjot21 <navjotsinghnarula@gmail.com>
2021-10-31 19:40:09 +0530

    LnB Assignmets

---
+++ 3meselarge_practice.py
@@ -0,0 +1,20 @@
+#to make a program to make the number lagest among 3
+a=int(input("enter the number a: "))
+b=int(input("enter the number b: "))
+c=int(input("enter the number c: "))
+if a>b:
+    if b>c:
+        print(a)
+    else:
+        if c>a:
+         print(c)
+
+else:
+    if b>c:
+        print(b)
+    else:
+             print(c)
+
+
+
+
